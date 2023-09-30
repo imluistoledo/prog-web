682a680bb1aeedd158d8a58462015061e685c3bb
Luis Toledo <imluistoledo@gmail.com>
2023-09-30 18:50:05 -0500

    common js

---
+++ parcial-2/modulo-common/require.js
@@ -0,0 +1,10 @@
+//let areas = require("./areas.js")
+
+import * as areas from "./areas.js";
+let cuadrado = areas.areaCuadrado(4)
+console.log(cuadrado)
+
+import areaOfCircle from "cal_area_of_a_circle" 
+
+    const result = areaOfCircle(45)
+    console.log(result)
