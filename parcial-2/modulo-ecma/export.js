036000ddfe6c90605e9216ac70db536e6d099492
Luis Toledo <imluistoledo@gmail.com>
2023-10-02 07:49:31 -0500

    modulos ecma con export e import

---
+++ parcial-2/modulo-ecma/export.js
@@ -0,0 +1,12 @@
+export function areaCuadrado(lado) {
+    return lado**2
+}
+
+export function areaTriangulo(base, altura) {
+    return (base * altura)/2
+}
+
+//module.exports.areaCuadrado = areaCuadrado
+//module.exports.areaTriangulo = areaTriangulo
+
+//console.log(module)
---
+++ parcial-2/modulo-ecma/import.js
@@ -0,0 +1,9 @@
+//let areas = require("./areas.js")
+
+import * as areas from "./export.js";
+
+let cuadrado = areas.areaCuadrado(4)
+let triangulo = areas.areaTriangulo(10,4)
+
+console.log(cuadrado)
+console.log(triangulo)
