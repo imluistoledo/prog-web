524d4bf000a4066d2f08a5bb29589a411485142e
Luis Toledo <imluistoledo@gmail.com>
2023-10-03 18:16:14 -0500

    event bubbling stop propagation

---
+++ parcial-2/eventos/event-bubbling/3_stop/stop-prop.js
@@ -0,0 +1,7 @@
+const arrNiveles = document.querySelectorAll(".bubbling div")
+arrNiveles.forEach(nivel => {
+    nivel.addEventListener("click", (e) => {
+        //e.stopPropagation()
+        alert(`Has presionado el nivel: ${e.target.className}`)
+    })
+})
