5a4af39d6821a7eeaeeacb346895ad4533960666
Luis Toledo <imluistoledo@gmail.com>
2023-10-03 18:15:36 -0500

    event bubbling propagation

---
+++ parcial-2/eventos/event-bubbling/1_propagacion/propagacion.js
@@ -0,0 +1,11 @@
+nivel1.addEventListener("click", () => {
+    alert("Has seleccionado el nivel 1!")
+})
+
+nivel2.addEventListener("click", () => {
+    alert("Has seleccionado el nivel 2!")
+})
+
+nivel3.addEventListener("click", () => {
+    alert("Has seleccionado el nivel 3!")
+})
