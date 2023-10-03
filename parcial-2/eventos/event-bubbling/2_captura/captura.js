1574e86d5995b67db0d8f732bcf5916b5b776f43
Luis Toledo <imluistoledo@gmail.com>
2023-10-03 18:15:53 -0500

    event bubbling capture

---
+++ parcial-2/eventos/event-bubbling/2_captura/captura.js
@@ -0,0 +1,11 @@
+nivel1.addEventListener("click", () => {
+    alert("Has seleccionado el nivel 1!")
+}, {capture: true})
+
+nivel2.addEventListener("click", () => {
+    alert("Has seleccionado el nivel 2!")
+}, {capture: true})
+
+nivel3.addEventListener("click", () => {
+    alert("Has seleccionado el nivel 3!")
+}, {capture: true})
