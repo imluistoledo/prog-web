a72ed44d64340690a387b39e1346982deedd6514
Luis Toledo <imluistoledo@gmail.com>
2023-10-06 10:46:44 -0500

    mejor funcionalidad, falta imagenes

--- parcial-2/tic-tac-toe/app.js
+++ parcial-2/tic-tac-toe/app.js
@@ -5,12 +5,18 @@ arrCajas.forEach(caja => {
     caja.addEventListener("click", (e) => {
         // Continuar js
         if (e.target.innerText == "") {
-            // Controlas turno
+            // Controlar turno
             e.target.innerText = turno // cambiar por imagen
             if (turno == "x") {
+                //turno = "o"
+                e.target.classList.remove("o")
+                e.target.classList.add("x")
                 turno = "o"
             }
             else {
+                //turno = "x"
+                e.target.classList.remove("x")
+                e.target.classList.add("o")
                 turno = "x"
             }
         }
